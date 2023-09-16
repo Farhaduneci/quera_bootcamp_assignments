c4ed126e90d4c2a63763b6320fac02457ff8b83f
Farhad Uneci <farhaduneci@gmail.com>
2023-09-17 01:35:34 +0330

    Reformat A04 queries, Add .gitignore

--- assignments/A04/Q3.sql
+++ assignments/A04/Q3.sql
@@ -4,9 +4,9 @@ SELECT
     customers.phone
 FROM
     orders
-    JOIN customers ON orders.customer_id = customers.id
+INNER JOIN customers ON orders.customer_id = customers.id
 GROUP BY
-    customer_id
+    orders.customer_id
 ORDER BY
     COUNT(*) DESC
 LIMIT 1;
@@ -17,8 +17,10 @@ SELECT
     foods.name
 FROM
     orders
-    JOIN restaurant_foods ON orders.restaurant_food_id = restaurant_foods.id
-    JOIN foods ON restaurant_foods.food_id = foods.id
+INNER JOIN
+    restaurant_foods
+    ON orders.restaurant_food_id = restaurant_foods.id
+INNER JOIN foods ON restaurant_foods.food_id = foods.id
 GROUP BY
     foods.id
 ORDER BY
@@ -33,8 +35,10 @@ SELECT
     restaurants.name
 FROM
     orders
-    JOIN restaurant_foods ON orders.restaurant_food_id = restaurant_foods.id
-    JOIN restaurants ON restaurant_foods.restaurant_id = restaurants.id
+INNER JOIN
+    restaurant_foods
+    ON orders.restaurant_food_id = restaurant_foods.id
+INNER JOIN restaurants ON restaurant_foods.restaurant_id = restaurants.id
 GROUP BY
     restaurants.id
 ORDER BY
@@ -49,13 +53,14 @@ SELECT
     customers.phone
 FROM
     customers
-    JOIN orders ON customers.id = orders.customer_id
-    JOIN restaurant_foods ON orders.restaurant_food_id = restaurant_foods.id
-    JOIN restaurants ON restaurant_foods.restaurant_id = restaurants.id
+INNER JOIN orders ON customers.id = orders.customer_id
+INNER JOIN
+    restaurant_foods
+    ON orders.restaurant_food_id = restaurant_foods.id
+INNER JOIN restaurants ON restaurant_foods.restaurant_id = restaurants.id
 GROUP BY
     customers.id
 HAVING
     COUNT(DISTINCT restaurants.id) >= 5
 ORDER BY
     customers.name;
-
